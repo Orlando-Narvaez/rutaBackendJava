b2fd1703acb4f7dc409f35c76e33d4fccde41dcf
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-02-09 14:08:44 -0500

    Create Variables.java
    Variables en Java

---
+++ cursoIntroducciónJavaSE/holaMundo/src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables
+{
+    public static void main(String[] args)
+    {
+        //Declarando variable
+        int speed;
+        //Asiganamos valor a la variable
+        speed = 10;
+        System.out.println(speed);
+
+        int salary = 1000;
+        String employeeName = "Orlando Narvaez";
+        System.out.println(employeeName);
+    }
+}
