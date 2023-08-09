f56007ab777957d4cdc7b7faf6f37a29adf8485b
Orlando Narvaez Baracaldo <orlando1999narvaez@gmail.com>
2023-08-09 13:19:29 -0500

    Se finaliza el curso

--- cursoIntroducciónJavaSE/HolaMundoJava/src/Funciones.java
+++ cursoIntroducciónJavaSE/HolaMundoJava/src/Funciones.java
@@ -1,37 +1,64 @@
-public class Funciones
-{
-    public static void main(String[] args)
-    {
-        double x = 2.1;
-        double y = 3;
+public class Funciones {
 
+    /*
+     * Linea 1
+     * Linea 2
+     *
+     * */
+
+    /**
+     * Javadoc
+     * */
+    public static void main(String[] args) {
+        double y = 3;
         //Area de un circulo
         //pi * r2
-        double area = circleArea(y);
-        System.out.println(area);
+        circleArea(y);
 
         //Area de una esfera
         //4*PI*r2
-        double areaEsfera = sphereArea(y);
-        System.out.println(areaEsfera);
+        sphereArea(y);
 
         //Volumen de una esfera
         //(4/3)*pi * r3
-        double valumenEsfera = 2;
+        sphereVolumen(y);
+
+        System.out.println("PESOS A DOLARES: " + converToDolar(1000, "COP"));
+
+
     }
 
-    public static double circleArea(double r)
-    {
+    public static double circleArea(double r){
         return Math.PI * Math.pow(r,2);
     }
 
-    public static double sphereArea(double r)
-    {
+    public static double sphereArea(double r){
         return 4 * Math.PI * Math.pow(r,2);
     }
 
-    public static double sphereVolumen(double r)
-    {
-        return (4/3) * Math.PI * Math.pow(y,3);
+    public  static double sphereVolumen(double r){
+        return (4/3) * Math.PI * Math.pow(r,3);
+    }
+
+
+    /**
+     * Descripción: Función que especificando su moneda convierte una cantidad de dinero a dolares
+     *
+     * @param quantity Cantidad de dinero
+     * @param currency Tipo de Moneda: Solo acepta MXN o COP
+     * @return quantity Devuelve la cantidad actualizada en Dolares
+     * */
+    public static double converToDolar(double quantity, String currency){
+        //MXN COP
+        switch (currency){
+            case "MXN":
+                quantity = quantity * 0.052;
+                break;
+            case "COP":
+                quantity = quantity * 0.00031;
+                break;
+        }
+
+        return quantity;
     }
 }
